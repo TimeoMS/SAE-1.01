3af409247d401354be2e1285ed50d1f767827e42
hugo-banti <hugo.banti@epitech.eu>
2025-12-29 18:17:10 +0100

    added game loop7

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include <ctime>
 #include <random>
 
-using std::cout, std::vector;
+using std::cout, std::vector, std::cin;
 
 const unsigned KReset(0);
 const unsigned KNoir(30);
@@ -91,7 +91,7 @@ struct Matrix // Pour l'optimisation
         size_t targetOrd = pos.ord;
         size_t targetAbs = pos.abs;
 
-        switch (direction)
+        switch (tolower(direction))
         {
         case 'r':
             if (targetAbs + 1 < n)
@@ -116,7 +116,7 @@ struct Matrix // Pour l'optimisation
         std::swap(at(pos), at(targetAbs, targetOrd));
     }
 
-    void checkMatches()
+    unsigned checkMatches()
     {
         // vecteur a optimiser pour stocker les positions
         vector<vector<bool>> toDelete(n, vector<bool>(n, false));
@@ -124,7 +124,7 @@ struct Matrix // Pour l'optimisation
         // verification des lignes
         for (size_t y(0); y < n; ++y) {
             for (size_t x(0); x < n - 3; ++x) {
-                if (at(x, y) == at(x + 1, y) && at(x, y) == at(x + 2, y)) {
+                if (at(x, y) != 0 && at(x, y) == at(x + 1, y) && at(x, y) == at(x + 2, y)) {
                     int lenth = x + 2;
                     while (lenth + 1 < n && at(x, y) == at(lenth + 1, y))
                         lenth++;
@@ -138,7 +138,7 @@ struct Matrix // Pour l'optimisation
         // verification des colonnes
         for (size_t x(0); x < n; ++x) {
             for (size_t y(0); y < n - 3; ++y) {
-                if (at(x, y) == at(x, y + 1) && at(x, y) == at(x, y + 2)) {
+                if (at(x, y) != 0 && at(x, y) == at(x, y + 1) && at(x, y) == at(x, y + 2)) {
                     int lenth = x + 2;
                     while (lenth + 1 < n && at(x, y) == at(x, lenth + 1))
                         lenth++;
@@ -149,20 +149,48 @@ struct Matrix // Pour l'optimisation
             }
         }
 
+        unsigned score = 0;
         // suppression des triples ou plus
-        for (size_t y(0); y < n; ++y)
-            for (size_t x(0); x < n; ++x)
-                if (toDelete[y][x])
+        for (size_t y(0); y < n; ++y) {
+            for (size_t x(0); x < n; ++x) {
+                if (toDelete[y][x]) {
                     at(x, y) = 0;
+                    score++;
+                }
+            }
+        }
+        return score;        
     }
 };
 
-int main(int argc, char const *argv[])
+void candyCrush(unsigned short nb_shots)
 {
     srand(time(0));
     Matrix m(10);
+    unsigned score = m.checkMatches();
     m.displayGrid();
-    m.checkMatches();
-    m.displayGrid();
+    for (int i = 0; i < nb_shots; ++i) {
+        maPosition pos;
+        char direction;
+        cout << "Enter Your Move (x y direction): ";
+        cin >> pos.abs >> pos.ord >> direction;
+        m.makeAMove(pos, direction);
+        score += m.checkMatches();
+        m.displayGrid();
+        cout << "Shots left: " << nb_shots - i - 1 << "\n";
+    }
+    cout << "Game Over your score is: " << score << "\n";
+}
+
+int main(int argc, char const **argv)
+{
+    if (argc == 2 && std::string(argv[1]) == "-h") {
+        cout << "Usage: \n"
+             << "  ./candycrush [nb_shots](def: 10)    Start the game\n"
+             << "  ./candycrush -h                     Show this help message\n";
+        return 0;
+    } else {
+        candyCrush(argv[1] ? std::stoi(argv[1]) : 10);
+    }
     return 0;
 }
